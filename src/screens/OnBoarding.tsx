a08907ae9c72b3e8b2e17b2dd351ba4b5de4c184
Murf-y <charbelfayad64@gmail.com>
2023-07-25 00:21:06 +0300

    fix: onBoarding style

--- src/screens/OnBoarding.tsx
+++ src/screens/OnBoarding.tsx
@@ -11,27 +11,27 @@ const OnBoarding = ({ navigation }: any) => {
   };
 
   return (
-    <SafeAreaView className="flex h-full w-full items-center justify-around flex-col bg-brand-lighter pl-12 pr-8">
+    <SafeAreaView className="flex h-full w-full items-center justify-around flex-col bg-brand-lighter px-8">
       <Image
         source={logo}
         style={{
           resizeMode: 'contain',
           width: '100%',
         }}></Image>
-      <View className="bg-brand w-full px-4 py-5 rounded-2xl flex items-center justify-between flex-col gap-5 relative">
-        <View className="absolute -top-6 left-1/2 w-1 h-1 flex items-center justify-center">
+      <View className="bg-brand w-full px-6 py-5 rounded-2xl flex items-center justify-between flex-col relative">
+        <View className="absolute -top-6 left-[55%] w-5 h-5 flex items-center justify-center">
           <ConfettiSvg width={65} height={65}></ConfettiSvg>
         </View>
 
-        <TextWrapper className="text-center text-white text-2xl font-bold">
+        <TextWrapper className="text-center text-white text-2xl font-bold mt-5">
           Unlock a Vibrant Campus Life!
         </TextWrapper>
-        <TextWrapper className="text-center text-white text-xs">
+        <TextWrapper className="text-center text-white text-xs  mt-5">
           Your key to unlocking a world of excitement and enriching experiences throughout your
           university journey. 🌟🎉
         </TextWrapper>
         <Pressable
-          className="bg-white rounded-3xl px-8 py-3"
+          className="bg-white rounded-3xl px-8 py-3  mt-5"
           onPress={() => {
             pressGetStarted();
           }}>
