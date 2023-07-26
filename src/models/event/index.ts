7e2c8e756382ad0e2090f4b2577dbda6e0013c87
Murf-y <charbelfayad64@gmail.com>
2023-07-26 11:56:53 +0300

    feat: add club and event model

---
+++ src/models/event/index.ts
@@ -0,0 +1,17 @@
+import { User } from '../user';
+
+export interface Event {
+  id: string;
+  eventName: string;
+  eventDescription: string;
+  startTime: Date;
+  endTime: Date;
+  clubId: string;
+  studentsAccepted: User[];
+  status: EventStatus;
+}
+
+export enum EventStatus {
+  Active = 'Active',
+  Cancelled = 'Cancelled',
+}
