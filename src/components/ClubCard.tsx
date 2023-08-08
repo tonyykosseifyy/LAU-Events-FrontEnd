b4fe7710de800ab7dda9d4dbb706c82929b56df9
Murf-y <charbelfayad64@gmail.com>
2023-08-08 20:27:09 +0300

    feat: hide club card when inactive

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -1,9 +1,10 @@
 import { View, Text, Pressable } from 'react-native';
 import React, { FC } from 'react';
 import TextWrapper from './TextWrapper';
-import { Club } from '../models/club';
+import { Club, ClubStatus } from '../models/club';
 import ArrowTopRightSVG from '../../assets/Icons/arrow_top_right.svg';
 import { useNavigation } from '@react-navigation/native';
+import clsx from 'clsx';
 
 const ClubCard: FC<{ club: Club; navigation: any }> = ({ club, navigation }) => {
   const pressOnClub = (club: Club) => {
@@ -11,8 +12,17 @@ const ClubCard: FC<{ club: Club; navigation: any }> = ({ club, navigation }) => 
       clubId: club.id,
     });
   };
+
+  console.log(club);
   return (
-    <View className="bg-white rounded-lg shadow-lg p-4 w-full flex flex-row justify-between items-center">
+    <View
+      className={clsx(
+        'rounded-lg shadow-lg p-4 w-full flex flex-row justify-between items-center',
+        {
+          'bg-gray-300': club.status === ClubStatus.INACTIVE,
+          'bg-white': club.status === ClubStatus.ACTIVE,
+        }
+      )}>
       <TextWrapper className="text-2xl text-black">{club.clubName}</TextWrapper>
       <Pressable
         className="p-4 bg-brand rounded-full"
