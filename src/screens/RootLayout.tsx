92e37d8e2ed6bb67e45f67fd8fa0007d0455e14b
Murf-y <charbelfayad64@gmail.com>
2023-08-12 14:13:59 +0300

    feat: add logout to admin

--- src/screens/RootLayout.tsx
+++ src/screens/RootLayout.tsx
@@ -21,6 +21,7 @@ import Signup from './Signup';
 import { EventDetails as UserEventDetails } from './user/EventDetails';
 import UserHome from './user/UserLayout';
 import DeclinedEvent from './user/DeclinedEvent';
+import Logout from './user/Logout';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -96,11 +97,13 @@ export default function RootLayout() {
               <RootStack.Screen name="ClubDetails" component={ClubDetails} />
               <RootStack.Screen name="AddEvent" component={AddEvent} />
               <RootStack.Screen name="EventDetails" component={EventDetails} />
+              <RootStack.Screen name="Logout" component={Logout} />
             </>
           ) : (
             <>
               <RootStack.Screen name="UserHome" component={UserHome} />
               <RootStack.Screen name="Home" component={Home} />
+              <RootStack.Screen name="Logout" component={Logout} />
               <RootStack.Screen name="DeclinedEvents" component={DeclinedEvent} />
               <RootStack.Screen name="EventDetails" component={UserEventDetails} />
             </>
